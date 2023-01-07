aaca8232584a242bc0f5f9aa1f5aedfed1337ea1
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-01-07 19:15:19 +0200

    added Popular css & js

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,3 +66,26 @@ setInterval(() => {
 		timerSeconds.innerHTML = seconds
 	}
 }, 1000)
+
+
+// Filter functionality
+const filterBtn = document.querySelectorAll("[data-filter-btn]")
+const filterItems = document.querySelectorAll("[data-filter]")
+
+let lastClickedBtn = filterBtn[0]
+
+const filter = function() {
+	lastClickedBtn.classList.remove("active")
+	this.classList.add("active")
+	lastClickedBtn = this
+
+	for (let i = 0; i < filterItems.length; i++) {
+		if (filterItems[i].dataset.filter === this.dataset.filterBtn) {
+			filterItems[i].style.display = "block"
+		} else {
+			filterItems[i].style.display = "none"
+		}
+	}
+}
+
+addEventOnElem(filterBtn, "click", filter)
